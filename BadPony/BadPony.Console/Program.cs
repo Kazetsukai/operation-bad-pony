3410a2750d38f0ed0d1e4abc39c662a0bf43a78a
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-04-14 09:28:11 +1200

    Laying out project, started writing console interface and game object

---
+++ BadPony/BadPony.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BadPony.Core;
+
+namespace BadPony.ConsoleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game gameInstance = new Game();
+
+            var location = gameInstance.GetAllObjects().FirstOrDefault();
+
+            while (true)
+            {
+                ShowRoom(location);
+
+                var line = Console.ReadLine();
+
+                if (line.ToLower() == "exit")
+                    return;
+
+
+            }
+        }
+
+        private static void ShowRoom(GameObject location)
+        {
+            if (location == null)
+            {
+                Console.WriteLine("You are floating in the void.");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ BadPony/BadPony.Core/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadPony.Core
+{
+    public class Game
+    {
+        private List<GameObject> _gameObjects = new List<GameObject>();
+
+        public IEnumerable<GameObject> GetAllObjects()
+        {
+            return _gameObjects.ToList();
+        }
+    }
+
+    public class GameObject
+    {
+        public GameObject(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public GameObject Container { get; set; }
+    }
+}
