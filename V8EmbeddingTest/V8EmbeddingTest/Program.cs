32be678e186eae86563eb1e6fe6fa19c286c0dc3
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-04-13 19:38:52 +1200

    Added V8 and made a JS REPL
    That was remarkably easy. I don't think REPLs can get much simpler than
    this.

---
+++ V8EmbeddingTest/V8EmbeddingTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ClearScript.V8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V8EmbeddingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v8Engine = new V8ScriptEngine();
+
+            string line = null;
+
+            while (line != String.Empty)
+            {
+                line = Console.ReadLine();
+                Console.WriteLine(v8Engine.ExecuteCommand(line));
+            }
+        }
+    }
+}
