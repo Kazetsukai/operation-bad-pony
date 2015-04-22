43e8408228790c60757af15308635de111621f77
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-04-23 11:26:52 +1200

    So angular

---
+++ BadPony/BadPony.WebApiHost/public/components/location/locationController.js
@@ -0,0 +1,7 @@
+﻿app.controller('LocationController', function ($scope, $http) {
+    $http({
+        url: "api/Location/1"
+    }).success(function(data, status, headers, config) {
+        $scope.location = data;
+    });
+});
