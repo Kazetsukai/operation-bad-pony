2a8a0a3f7e23ca6a0ffac1698fb63b70c187eb7f
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-04-24 16:32:14 +1200

    Moved move object message (hah)

---
+++ BadPony/BadPony.Core/MoveObjectMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace BadPony.Core
+{
+    public class MoveObjectMessage : IGameMessage
+    {
+        public int ObjectId { get; set; }
+        public int DestinationId { get; set; }
+        public int? OriginId { get; set; }
+    }
+}
