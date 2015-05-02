f31fc83959f549b86be5b949f1f0530b016dcbe3
Troy West <troy.west@enlighten.co.nz>
2015-05-03 00:49:16 +1200

    Automated table drawing in the AdminCLI

--- BadPony/BadPony.WebApiHost/AdminCLI.cs
+++ BadPony/BadPony.WebApiHost/AdminCLI.cs
@@ -11,12 +11,13 @@ namespace BadPony.WebApiHost
 {
     class AdminCLI
     {
-        private static bool exit = false;        
+        private static bool exit = false;
         private static string prompt = "BP: ";
         private static string adminResources = "./AdminResources/";
         private static Logger logger = LogManager.GetCurrentClassLogger();
         private string input;
         private string[] command;
+        private const int TAB = 8;
 
         public void StartCLI(Game game)
         {
@@ -32,6 +33,10 @@ namespace BadPony.WebApiHost
                     {
                         case "bad":
                             displayPony(command);
+                            break;
+                        case "dt":
+                            List<string[]> list = new List<string[]> { new string[] { "12345678", "123", "2312333123123123123213", "test" }, new string[] { "126789", "12312", "312312", "234" }, new string[] { "123", "21312", "12", "234" } };
+                            drawTable(new string[4] { "1234", "4325", "2344", "test" }, list);
                             break;
                         case "exit":
                             logger.Info("\tWAPI\tExit command entered on CLI");
@@ -50,7 +55,7 @@ namespace BadPony.WebApiHost
                             Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("{0} is not a known command. Type help for a list of available commands.", command[0]);
                             continue;
-                    } 
+                    }
                 }
                 Array.Clear(command, 0, command.Length);
             }
@@ -59,7 +64,7 @@ namespace BadPony.WebApiHost
         private static void displayPony(string[] command)
         {
             if (command.Length > 1 && command[1] == "pony" || command[1] == "brony")
-            {                
+            {
                 LoadArt(command[1]);
             }
             else
@@ -127,40 +132,116 @@ namespace BadPony.WebApiHost
 
             if (command[1] == "players")
             {
-                Console.WriteLine("ID\tName\t\tUsername\tLocation\n-----------------------------------------------------------");
+                string[] headers = new string[] { "ID", "Name", "Username", "Location" };              
                 List<Player> players = game.GetAllObjects().OfType<Player>().ToList();
+                List<string[]> playerData = new List<string[]>();
                 foreach (Player player in players)
                 {
-                    Console.WriteLine(player.Id + "\t" + player.Name + "\t\t" + player.UserName +"\t\t"+ game.GetObject(player.ContainerId).Name);
+                    playerData.Add(new string[]{ player.Id.ToString(), player.Name, player.UserName, game.GetObject(player.ContainerId).Name });                    
                 }
+                drawTable(headers, playerData);
             }
             else if (command[1] == "locations")
             {
-                Console.WriteLine("ID\tName\t\t\tExits\n-----------------------------------------------------------");
+                string[] headers = new string[] { "ID", "Name", "Exits" };                
                 List<Location> locations = game.GetAllObjects().OfType<Location>().ToList();
-                foreach(Location location in locations)
+                List<string[]> locationData = new List<string[]>();
+                foreach (Location location in locations)
                 {
-                    string output = location.Id + "\t" + location.Name + "\t\t\t";
+                    string output = "";
                     List<Door> exits = game.GetContainedObjects(location.Id).OfType<Door>().ToList();
                     foreach (var exit in exits)
                     {
                         output += exit.Id + ", ";
                     }
-                    output = output.Remove(output.Length - 2);
-                    Console.WriteLine(output);
+                    if (output.Length > 0)
+                    {
+                        output = output.Remove(output.Length - 2);
+                    }
+                    locationData.Add(new string[] { location.Id.ToString(), location.Name, output });                    
                 }
+                drawTable(headers, locationData);
             }
-            else if(command[1] == "doors")
+            else if (command[1] == "doors")
             {
-                Console.WriteLine("ID\tName\t\t\tLocations\n-----------------------------------------------------------");
+                string[] headers = new string[] { "ID", "Name", "Location" };                
                 List<Door> doors = game.GetAllObjects().OfType<Door>().ToList();
+                List<string[]> doorData = new List<string[]>();
                 foreach (var door in doors)
                 {
-                    Console.WriteLine(door.Id + "\t" + door.Name + "\t" + game.GetObject(door.DestinationId).Name);                    
+                    doorData.Add(new string[] {door.Id.ToString() , door.Name , game.GetObject(door.DestinationId).Name});
                 }
+                drawTable(headers, doorData);
+            }
+            else if (command[1] == "all")
+            {
+                string[] headers = new string[] { "ID", "Name", "Location" };                
+                List<GameObject> all = game.GetAllObjects().ToList();
+                List<string[]> allData = new List<string[]>();
+                foreach (var item in all)
+                {
+                    allData.Add(new string[] {item.Id.ToString(), item.Name , game.GetObject(item.ContainerId).Name});
+                }
+                drawTable(headers, allData);
+            }            
+        }
+
+
+
+        private static void drawTable(string[] headers, List<string[]> lines)
+        {
+            string heading = "";
+            int count = 0;
+            int[] lengths = new int[headers.Length];
+            string body = "";
+
+            foreach (string header in headers)
+            {
+                int longest = 1;
+                foreach (var line in lines)
+                {
+                    if (count < line.Length)
+                    {
+                        var x = line[count].Length / 8;
+                        if (x % TAB != 0)
+                        {
+                            x++;
+                        }
+                        if (x > longest)
+                        {
+                            longest = x;
+                        }
+                    }
+                }
+                lengths[count] = longest;
+                count++;
+                heading += header;
+                for (int i = 0; i < longest - (header.Length / TAB); i++)
+                {
+                    heading += "\t";
+                }
+                
+            }
+            foreach (var line in lines)
+            {
+                count = 0;
+                foreach (var entry in line)
+                {
+                    body += entry;
+                    for (int i = 0; i < lengths[count] - (entry.Length / TAB); i++)
+                    {
+                        body += "\t";
+                    }
+                    count++;
+                }
+                body += "\n";
             }
 
-            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine(heading);
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(body);
+            Console.WriteLine("-------------------------------------------------------------------------------");
         }
+
     }
 }
